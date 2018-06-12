f0d47feb3cd3c3095e7087dc3376d4881a1e2034
Dingwen Tao <guanyisun@Guanyis-MacBook-Air.local>
2018-06-12 07:40:32 -0700

    finished make and test

---
+++ MILOF/test/milof_test.py
@@ -0,0 +1,7 @@
+import sys
+import time
+import os
+import MiLOF
+
+MiLOF.MILOF_Kmeans_Merge("parser.cfg")
+print ("online anomaly detection test passed.\n")
---
+++ MILOF/test/parser_test.py
@@ -0,0 +1,7 @@
+import sys
+import time
+import os
+import strmParser
+
+strmParser.Parser("parser.cfg")
+print("Streaming event parser test passed.\n")
