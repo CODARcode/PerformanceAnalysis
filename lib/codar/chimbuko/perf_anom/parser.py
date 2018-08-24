7327c3bf5986f164b9f549307e687c746c0ac46d
Gyorgy <gmatyasfalvi@bnl.gov>
2018-08-24 08:58:33 -0400

    Added run_test.sh, worked on parser initialization.

--- lib/codar/chimbuko/perf_anom/parser.py
+++ lib/codar/chimbuko/perf_anom/parser.py
@@ -12,16 +12,23 @@ class Parser():
     def __init__(self, configFile):
       self.config = configparser.ConfigParser()
       self.config.read(configFile)
-      self.fileType = config['Parser']['FileType']
-      self.inputFile = config['Parser']['InputFile'] # input file path
-      self.parseMode = config['Parser']['ParseMode']
+      self.fileType = self.config['Parser']['FileType']
+      self.inputFile = self.config['Parser']['InputFile'] # input file path
+      self.parseMode = self.config['Parser']['ParseMode']
       self.stream = None
       self.status = None
+      self.bpAttrib = None
+      self.bpNumAttrib = None
       
-      if fileType == "bp":
+      if self.fileType == "bp":
           ad.read_init("BP", parameters="verbose=3") # initialize adios streaming mode
           if self.parseMode == "stream":
-              self.stream = ad.file(inputFile, "BP", is_stream=True, timeout_sec=10.0)
+              self.stream = ad.file(self.inputFile, "BP", is_stream=True, timeout_sec=10.0)
+              self.bpAttrib = stream.attr
+              self.bpNumAttrib = stream.nattrs
+              print("Adios stream ready... \n\n\n")
+              print("Attribute names: \n", self.bpAttrib)
+              
               
     
     def getStream(self):
