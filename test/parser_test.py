63edb3e3556aecd26e36d4d3939ef4a4c449366e
Gyorgy <gmatyasfalvi@bnl.gov>
2018-08-24 08:20:02 -0400

    Added config file.

--- test/parser_test.py
+++ test/parser_test.py
@@ -4,4 +4,10 @@ Authors: Shinjae Yoo (sjyoo@bnl.gov), Gyorgy Matyasfalvi (gmatyasfalvi@bnl.gov)
 Create: August, 2018
 """
 
+import sys
+import time
+import os
 import parser
+import event
+
+prs = parser.Parser("chimbuko.cfg")
