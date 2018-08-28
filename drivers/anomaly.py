9b6c5070906d2e0fb5780ec83c5b45e1c9cf1305
Gyorgy <gmatyasfalvi@bnl.gov>
2018-08-28 18:57:12 -0400

    Added new direcotry drivers/, this directory will contain various anomaly detection methods. Continued work on anomaly.py driver.

--- drivers/anomaly.py
+++ drivers/anomaly.py
@@ -18,3 +18,12 @@ import outlier
 prs = parser.Parser(sys.argv[1])
 evn = event.Event(prs.getFunMap(), sys.argv[1])
 
+Points = Point()
+clf = LocalOutlierFactor(n_neighbors=kpar, algorithm="kd_tree", leaf_size=30, metric='euclidean')
+clf.fit(datastream)
+Points.LOF = [-x for x in clf.negative_outlier_factor_.tolist()]
+Points.lrd = clf._lrd.tolist()
+dist, ind = clf.kneighbors()
+Points.kdist = dist.tolist()
+Points.knn = ind.tolist()
+return Points
