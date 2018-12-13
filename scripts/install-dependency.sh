8b0dd0c5bcc5d5f1c0840e6d4a7743ae8333e68e
Gyorgy <gmatyasfalvi@bnl.gov>
2018-12-13 11:59:44 -0500

    Reorganized files for December release.

--- scripts/install-dependency.sh
+++
@@ -1,1 +0,0 @@
-pip install numpy scipy python-slugify ijson nose coverage pandas sklearn 
--- scripts/process_tJson2eeJson.sh
+++
@@ -1,4 +0,0 @@
-for i in `ls test-for-ml-lammps-*.json | sed "s/.json//"`; do 
-  echo $i; 
-  python3 /home/sjyoo/codar/pa/perf_anom/src/trace2ee.py $i;
-done
