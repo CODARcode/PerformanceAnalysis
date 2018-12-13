0a620306816fa9df0809a7703689c1fe68e09828
Gyorgy <gmatyasfalvi@bnl.gov>
2018-12-13 08:49:38 -0500

    Adding unit tests.

---
+++ lib/codar/chimbuko/perf_anom/test_parser.py
@@ -0,0 +1,36 @@
+import sys
+import parser
+import unittest
+
+
+class TestGetStream(unittest.TestCase):
+    """Test parser for correctly catching the Adios stream.
+    """
+        
+    def test_getStream(self):
+        """Test getStream() function, which performs an Adios advance operation.
+        """
+        
+        prs = parser.Parser("test.cfg")
+        parseMethod = prs.getParseMethod()
+        
+        ctrl = 0
+        count = 0
+        while ctrl >= 0:
+            prs.getStream()
+            ctrl = prs.getStatus()
+            count += 1
+        
+        self.assertEqual(count, 11)
+
+
+if __name__ == '__main__':
+    unittest.main()    
+    
+    
+    
+    
+    
+    
+    
+    
