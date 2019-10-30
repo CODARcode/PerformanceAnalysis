d2a377ea912110284870fe1878ae1883db3b5cd1
Sungsoo Ha <sungsooha@visws.csi.bnl.gov>
2019-10-30 11:34:07 -0400

    update the latest scripts used on Summit

--- cpp/scripts/summit/run_jobs.sh
+++ cpp/scripts/summit/run_jobs.sh
@@ -1,108 +1,105 @@
 #!/bin/bash
-# script to generate lsf files to summit jobs on Summit
-#
-# - according to a given MPI ranks, it will compute the number of 
-#   required summit nodes.
-#
-# - there are four cases: 
-#   1. NWChem only: run pure NWChem
-#   2. NWChem + TAU: run TAU instrumented NWChem
-#   3. NWChem + TAU + Chimbuko: run TAU instrumented NWChem with anomaly detection
-#   4. NWChem + TAU + sstSinker: run TAU instrumented NWChem with data sinker 
-#     Note that sstSinker is to analyze ADIOS performance (SST vs. BP)
-#
-#
 
-Usages () {
-   echo "run_jobs.sh CASE MIN MAX STEP"
-   echo "- CASE: case id in [0, 1, 2, 3]"
-   echo "- MIN: the minimum MPI ranks"
-   echo "- MAX: the maximum MPI ranks"
-   echo "- STEP: MPI rank step size"
-   echo "- e.g. run_jobs.sh 2 200 1000 200"
-   exit 1
-}
+ROOT_DIR=`pwd`
+BATCH_DIR="${ROOT_DIR}/bp-504"
+mkdir -p $BATCH_DIR
+cp run_webserver.sh ${BATCH_DIR}
+cp shutdown_webserver.sh ${BATCH_DIR}
+cp run_pserver.sh ${BATCH_DIR}
 
-if [ $# -ne 4 ]
-then
-   Usages
-fi
+APPNAME=nwchem-504
 
-# determine MPI ranks range (min, max, step) 
-# Note that MPI ranks must be divisible by 20!
-CASE=$1
-RANKS_MIN=$2
-RANKS_MAX=$3
-RANKS_INC=$4
+# true to use TAU instrumented NWChem
+USE_TAU=true
+# true to use full TAU instrumented NWChem
+USE_FULL=true
 
-if [ $CASE -eq 0 ]
-then
-   JOB=4pgs-only
-   RUN_SCRIPT=run_nwchem_only
-elif [ $CASE -eq 1 ]
-then
-   JOB=4pgs-tau
-   RUN_SCRIPT=run_nwchem_tau
-elif [ $CASE -eq 2 ]
-then 
-   JOB=4pgs-all
-   RUN_SCRIPT=run_nwchem_all
-elif [ $CASE -eq 3 ]
-then 
-   JOB=4pgs-sst
-   RUN_SCRIPT=run_nwchem_sstSinker
-else
-   echo "Invalid case number: $CASE"
-   exit 1
-fi
+ADIOS=BPFile
+ADIOS_INTERVAL=1000000
+USE_C2=false
 
-echo "============================"
-echo "JOB: $JOB"
-echo "RUN_SCRIPT: $RUN_SCRIPT"
-echo "MPI ranks: [$RANKS_MIN, $RANKS_MAX, $RANKS_INC]"
-echo "============================"
+mkdir -p $BATCH_DIR
 
-# loop over the MPI ranks
-for (( ranks=$RANKS_MIN; ranks<=$RANKS_MAX; ranks+=$RANKS_INC ))
+
+for (( nnodes=4; nnodes<=128; nnodes*=2 ))
 do
-  # job name
-  JOBNAME=${JOB}-${ranks}
-  # number of nodes ( +1 only for all)
-  #NNODES=$(( ($ranks + 20 - 1)/20  ))
-  NNODES=$(( ($ranks + 20 - 1)/20 + 1 ))
+  cd $ROOT_DIR
+  # given condition, it modifies run.lsf file from top to bottom.
+  # so that, it is important to check the below codes whenever
+  # run.lsf file is modified.
 
-  # number of resource sets (assume num ranks are divisible by 20)
-  NSETS=$(( $ranks/20 ))
-  # number of MPI tasks (ranks) per resource set
-  NMPIS=20
-  # number of CPU cores per resource set
-  NCORES=20
-  # number of GPUs per resource set
-  NGPUS=0
-  # number of resource set per host (node)
-  NSETSNODE=1
-
-  echo -n "$JOBNAME: $NNODES: "
-  echo -n "-n $NSETS -a $NMPIS -c $NCORES -g $NGPUS -r $NSETSNODE"
-  echo 
-
-  # create a job
-  mkdir -p $JOB
-  cd $JOB
+  # number of MPI processors
+  n_mpi=$(( nnodes * 20 ))
+  if $USE_TAU; then
+    jobname="${APPNAME}-${nnodes}-${ADIOS}"
+  else
+    jobname="${APPNAME}-${nnodes}"
+  fi
+  actual_nnodes=$nnodes
+  if [ $USE_TAU == false ] || [ "${ADIOS}" == "BPFile"  ]; then
+    actual_nnodes=$nnodes
+  else
+    actual_nnodes=$(( $nnodes + 2 ))
+  fi
   
-  LSF="${RUN_SCRIPT}_${JOBNAME}.lsf"
-  cp ../${RUN_SCRIPT}.lsf "${LSF}"
-  sed -i "s|NSETSNODE|$NSETSNODE|g" "${LSF}"
-  sed -i "s|NNODES|$NNODES|g" "${LSF}"
-  sed -i "s|JOBNAME|$JOBNAME|g" "${LSF}"
-  sed -i "s|JOB|$JOB|g" "${LSF}"
-  sed -i "s|NSETS|$NSETS|g" "${LSF}"
-  sed -i "s|NMPIS|$NMPIS|g" "${LSF}"
-  sed -i "s|NCORES|$NCORES|g" "${LSF}"
-  sed -i "s|NGPUS|$NGPUS|g" "${LSF}"
-  sed -i "s|SINKMODE|$SINKMODE|g" "${LSF}"
-  # submit the job
-  bsub "${LSF}"
-  sleep 1 
-  cd .. 
+  cp run.lsf "${BATCH_DIR}/${jobname}.lsf"
+  lsf="${jobname}.lsf"
+  if $USE_C2; then
+    n_rs=$(( $n_mpi / 10 ))
+    ad_arg="-n ${n_rs} -a 10 -c 1 -g 0 -r 2"
+    app_arg="-n ${n_mpi} -a 1 -c 2 -g 0 -r 20"
+  else
+    ad_arg="-n ${n_mpi} -a 1 -c 1 -g 0 -r 20"
+    app_arg="-n ${n_mpi} -a 1 -c 1 -g 0 -r 20"
+  fi
+  cd $BATCH_DIR
+  # LSF directives
+  sed -i "5s|nnodes 1|nnodes ${actual_nnodes}|" $lsf
+  sed -i "6s|single-node|${jobname}|" $lsf
+  sed -i "7s|single-node|${jobname}|" $lsf
+  sed -i "8s|single-node|${jobname}|" $lsf
+  # Select NWChem binary
+  echo -n "MPI: ${n_mpi}, "
+  if [ $USE_TAU == false ]; then
+    echo "USE non-TAU instrumented NWChem"
+    sed -i "28s|nwchem-1-tau-ga|nwchem-1|" $lsf
+  elif [ $USE_FULL == false ]; then
+    echo "USE TAU instrumented NWChem (filtered)"
+  else
+    echo "USE TAU instrumented NWChem (full)"
+    sed -i "28s|nwchem-1-tau-ga|nwchem-1-tau-full|" $lsf
+  fi  
+  # Select Working directory
+  sed -i "55s|single_node|${jobname}|" $lsf
+  sed -i "56s|single_node|${jobname}|" $lsf
+  sed -i "57s|single_node|${jobname}|" $lsf  
+  # Set ADIOS-TAU mode and interval
+  sed -i "69s|1000000|${ADIOS_INTERVAL}|" $lsf
+  sed -i "70s|SST|${ADIOS}|" $lsf
+  sed -i "71s|APP_NMPIS=20|APP_NMPIS=${n_mpi}|" $lsf
+  if [ $USE_TAU == false ] || [ "${ADIOS}" = "BPFile" ]; then
+    # disable visualization server
+    sed -i "132s|true|false|" $lsf
+    # disable parameter server
+    sed -i "157s|true|false|" $lsf
+    # disable anomaly detector
+    sed -i "191s|true|false|" $lsf
+    # finalize
+    sed -i "199s|wait|#wait|" $lsf
+    sed -i "201s|true|false|" $lsf
+  else
+    # run visualization server with maximum celery concurrency
+    sed -i "138s|-c 1|-c 42|" $lsf
+    #sed -i "152s|--concurrency=10|--concurrency=100|" $lsf
+    # run parameter server
+    sed -i "162s|-c 1| -c 42|" $lsf
+    sed -i "162s|run_pserver.sh 4|run_pserver.sh 160|" $lsf
+    # run anomaly detector
+    sed -i "192s|-n 20 -a 1 -c 1 -g 0 -r 20|${ad_arg}|" $lsf
+  fi
+  # nwchem
+  sed -i "197s|-n 20 -a 1 -c 1 -g 0 -r 20|${app_arg}|" $lsf
+
+  bsub $lsf
+  sleep 1
 done
