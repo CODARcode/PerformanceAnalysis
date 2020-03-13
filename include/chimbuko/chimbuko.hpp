5e8f735c9bc3161d54bdbf59bddda8e55446ccb8
Christopher Kelly <ckelly@bnl.gov>
2020-03-13 13:42:53 -0400

    Broke up Chimbuko class run command into separate private functions for clarity

--- include/chimbuko/chimbuko.hpp
+++ include/chimbuko/chimbuko.hpp
@@ -4,14 +4,13 @@
 
 namespace chimbuko {
 
-class Chimbuko
-{
-public:
+  class Chimbuko{
+  public:
     Chimbuko();
     ~Chimbuko();
 
     void init_io(int rank, IOMode mode, std::string outputPath, 
-        std::string addr, unsigned int winSize=0);
+		 std::string addr, unsigned int winSize=0);
     void init_parser(std::string data_dir, std::string inputFile, std::string engineType);
     void init_event(bool verbose=false);
     void init_outlier(int rank, double sigma, std::string addr="");
@@ -24,18 +23,37 @@ public:
     int get_step() const { return m_parser->getCurrentStep(); }
 
     void run(int rank, 
-        unsigned long long& n_func_events, 
-        unsigned long long& n_comm_events,
-        unsigned long& n_outliers,
-        unsigned long& frames,
+	     unsigned long long& n_func_events, 
+	     unsigned long long& n_comm_events,
+	     unsigned long& n_outliers,
+	     unsigned long& frames,
 #ifdef _PERF_METRIC
-        std::string perf_outputpath="",
-        int         perf_step=10,
+	     std::string perf_outputpath="",
+	     int         perf_step=10,
 #endif
-        bool only_one_frame=false,
-        int interval_msec=0);
+	     bool only_one_frame=false,
+	     int interval_msec=0);
 
-private:
+  private:
+    /**
+     * @brief Signal the parser to parse the adios2 timestep
+     * @param[out] step index
+     * @param[out] number of func events parsed
+     * @param[out] number of comm events parsed
+     * @return false if unsuccessful, true otherwise
+     */
+    bool parseInputStep(int &step, 
+			unsigned long long& n_func_events, 
+			unsigned long long& n_comm_events);
+
+
+    /*
+     * @brief Extract parsed events and insert into the event manager
+     * @param rank The MPI rank of the process
+     * @param step The adios2 stream step index
+     */
+    void extractEvents(int rank, int step);
+  
     // int m_rank;
     // std::string m_engineType;  // BPFile or SST
     // std::string m_data_dir;    // *.bp location
@@ -52,6 +70,6 @@ private:
     // call stack violation and the priority queue approach is not the ultimate
     // solution. So, it is deprecated and hope the problem is solved in TAU side.
     // PQUEUE pq;
-};
+  };
 
 }
--- src/chimbuko.cpp
+++ src/chimbuko.cpp
@@ -73,139 +73,145 @@ void Chimbuko::finalize()
     m_io = nullptr;
 }
 
+//Returns false if beginStep was not successful
+bool Chimbuko::parseInputStep(int &step, 
+			      unsigned long long& n_func_events, 
+			      unsigned long long& n_comm_events){
+  if (!m_parser->getStatus()) return false;
+  m_parser->beginStep();
+  if (!m_parser->getStatus()) return false;
+    
+  // get trace data via SST
+  step = m_parser->getCurrentStep();
+  m_parser->update_attributes();
+  m_parser->fetchFuncData();
+  m_parser->fetchCommData();
+  m_parser->fetchCounterData();
+
+  // early SST buffer release
+  m_parser->endStep();
+
+  // count total number of events
+  n_func_events += (unsigned long long)m_parser->getNumFuncData();
+  n_comm_events += (unsigned long long)m_parser->getNumCommData();
+  return true;
+}
+
+//Extract parsed events and insert into the event manager
+void Chimbuko::extractEvents(int rank, int step){
+  size_t idx_funcData=0, idx_commData = 0;
+  const unsigned long *funcData = nullptr;
+  const unsigned long *commData = nullptr;
+
+  //During the timestep a number of function and perhaps also comm events occurred
+  //The parser stores these events separately in order of their timestamp
+  //We want to generate a combined list of both function and comm events ordered by their timestamp	
+  funcData = m_parser->getFuncData(idx_funcData);
+  commData = m_parser->getCommData(idx_commData);
+  
+  while (funcData != nullptr || commData != nullptr){
+    // Determine event to handle
+    // - if both, funcData and commData, are available, select one that occurs earlier.
+    // - priority is on funcData because communication might happen within a function.
+    // - in the future, we may not need to process on commData (i.e. exclude it from execution data).
+    const unsigned long* data = nullptr;
+    bool is_func_event = true;
+    if ( (funcData != nullptr && commData == nullptr) ) {
+      data = funcData;
+      is_func_event = true;
+    }
+    else if (funcData == nullptr && commData != nullptr) {
+      data = commData;
+      is_func_event = false;
+    }
+    else if (funcData[FUNC_IDX_TS] <= commData[COMM_IDX_TS]) {
+      data = funcData;
+      is_func_event = true;
+    }
+    else {
+      data = commData;
+      is_func_event = false;
+    }
+
+    // Create event
+    const Event_t ev = Event_t(
+			       data,
+			       (is_func_event ? EventDataType::FUNC: EventDataType::COMM),
+			       (is_func_event ? idx_funcData: idx_commData),
+			       (is_func_event ? generate_event_id(rank, step, idx_funcData): "event_id")
+			       );
+    // std::cout << ev << std::endl;
+
+    // Validate the event
+    // NOTE: in SST mode with large rank number (>=10), sometimes I got
+    // very large number of pid, rid and tid. This issue was also observed in python version.
+    // In BP mode, it doesn't have such problem. As temporal solution, we skip those data and
+    // it doesn't cause any problems (e.g. call stack violation). Need to consult with 
+    // adios team later
+    if (!ev.valid() || ev.pid() > 1000000 || (int)ev.rid() != rank || ev.tid() >= 1000000){
+      std::cout << "\n***** Invalid event *****\n";
+      //std::cout << "[" << rank << "] " << ev << std::endl;
+    }else{
+      EventError err = m_event->addEvent(ev);
+      // if (err == EventError::CallStackViolation)
+      // {
+      //     std::cout << "\n***** Call stack violation *****\n";
+      // }
+    }
+    
+    // go next event
+    if (is_func_event) {
+      idx_funcData++;
+      funcData = m_parser->getFuncData(idx_funcData);
+    } else {
+      idx_commData++;
+      commData = m_parser->getCommData(idx_commData);
+    }
+  }//while loop over func and comm data
+}
+  
+
+
+
 void Chimbuko::run(int rank, 
-    unsigned long long& n_func_events, 
-    unsigned long long& n_comm_events,
-    unsigned long& n_outliers,
-    unsigned long& frames,
+		   unsigned long long& n_func_events, 
+		   unsigned long long& n_comm_events,
+		   unsigned long& n_outliers,
+		   unsigned long& frames,
 #ifdef _PERF_METRIC
-    std::string perf_outputpath,
-    int         perf_step,
+		   std::string perf_outputpath,
+		   int         perf_step,
 #endif
-    bool only_one_frame,
-    int interval_msec)
-{
-    int step = 0; 
-    size_t idx_funcData = 0, idx_commData = 0;
-    const unsigned long *funcData = nullptr, *commData = nullptr, *counterData = nullptr;
+		   bool only_one_frame,
+		   int interval_msec){
+  int step = 0; 
 
 #ifdef _PERF_METRIC
-    std::string ad_perf = "ad_perf_" + std::to_string(rank) + ".json";
+  std::string ad_perf = "ad_perf_" + std::to_string(rank) + ".json";
 #endif
 
-    //Loop until we lose connection with the application
-    while ( m_parser->getStatus() ) 
-    {
-        m_parser->beginStep();
-        if (!m_parser->getStatus())
-            break;
+  //Loop until we lose connection with the application
+  while ( parseInputStep(step, n_func_events, n_comm_events) ) {
+    //Extract parsed events into event manager
+    extractEvents(rank, step);
 
-        // get trace data via SST
-        step = m_parser->getCurrentStep();
-        m_parser->update_attributes();
-        m_parser->fetchFuncData();
-        m_parser->fetchCommData();
-	m_parser->fetchCounterData();
+    //Run the outlier detection algorithm on the events
+    n_outliers += m_outlier->run(step);
+    frames++;
 
-        // early SST buffer release
-        m_parser->endStep();
-
-        // count total number of events
-        n_func_events += (unsigned long long)m_parser->getNumFuncData();
-        n_comm_events += (unsigned long long)m_parser->getNumCommData();
-
-        // prepare to analyze current step
-        idx_funcData = idx_commData = 0;
-        funcData = nullptr;
-        commData = nullptr;
-
-	//Add events to the ADEvent instance
-	
-	//During the timestep a number of function and perhaps also comm events occurred
-	//The parser stores these events separately in order of their timestamp
-	//We want to generate a combined list of both function and comm events ordered by their timestamp	
-        funcData = m_parser->getFuncData(idx_funcData);
-        commData = m_parser->getCommData(idx_commData);
-        while (funcData != nullptr || commData != nullptr)
-        {
-            // Determine event to handle
-            // - if both, funcData and commData, are available, select one that occurs earlier.
-            // - priority is on funcData because communication might happen within a function.
-            // - in the future, we may not need to process on commData (i.e. exclude it from execution data).
-            const unsigned long* data = nullptr;
-            bool is_func_event = true;
-            if ( (funcData != nullptr && commData == nullptr) ) {
-                data = funcData;
-                is_func_event = true;
-            }
-            else if (funcData == nullptr && commData != nullptr) {
-                data = commData;
-                is_func_event = false;
-            }
-            else if (funcData[FUNC_IDX_TS] <= commData[COMM_IDX_TS]) {
-                data = funcData;
-                is_func_event = true;
-            }
-            else {
-                data = commData;
-                is_func_event = false;
-            }
-
-            // Create event
-            const Event_t ev = Event_t(
-                data,
-                (is_func_event ? EventDataType::FUNC: EventDataType::COMM),
-                (is_func_event ? idx_funcData: idx_commData),
-                (is_func_event ? generate_event_id(rank, step, idx_funcData): "event_id")
-            );
-            // std::cout << ev << std::endl;
-
-            // Validate the event
-            // NOTE: in SST mode with large rank number (>=10), sometimes I got
-            // very large number of pid, rid and tid. This issue was also observed in python version.
-            // In BP mode, it doesn't have such problem. As temporal solution, we skip those data and
-            // it doesn't cause any problems (e.g. call stack violation). Need to consult with 
-            // adios team later
-            if (!ev.valid() || ev.pid() > 1000000 || (int)ev.rid() != rank || ev.tid() >= 1000000)
-            {
-                std::cout << "\n***** Invalid event *****\n";
-                //std::cout << "[" << rank << "] " << ev << std::endl;
-            }
-            else
-            {
-                EventError err = m_event->addEvent(ev);
-                // if (err == EventError::CallStackViolation)
-                // {
-                //     std::cout << "\n***** Call stack violation *****\n";
-                // }
-            }
-
-            // go next event
-            if (is_func_event) {
-                idx_funcData++;
-                funcData = m_parser->getFuncData(idx_funcData);
-            } else {
-                idx_commData++;
-                commData = m_parser->getCommData(idx_commData);
-            }
-        }
-        
-        n_outliers += m_outlier->run(step);
-        frames++;
-
-	//Pull out all complete events and write to output
-        m_io->write(m_event->trimCallList(), step);
+    //Pull out all complete events and write to output
+    m_io->write(m_event->trimCallList(), step);
 	
 #ifdef _PERF_METRIC
-        // dump performance metric event 10 steps
-        if ( perf_outputpath.length() && perf_step > 0 && (step+1)%perf_step == 0 ) {
-            m_outlier->dump_perf(perf_outputpath, ad_perf);
-        }
+    // dump performance metric event 10 steps
+    if ( perf_outputpath.length() && perf_step > 0 && (step+1)%perf_step == 0 ) {
+      m_outlier->dump_perf(perf_outputpath, ad_perf);
+    }
 #endif
-        if (only_one_frame)
-            break;
+    if (only_one_frame)
+      break;
 
-        if (interval_msec)
-            std::this_thread::sleep_for(std::chrono::microseconds(interval_msec));
-    } // end of parser while loop
+    if (interval_msec)
+      std::this_thread::sleep_for(std::chrono::microseconds(interval_msec));
+  } // end of parser while loop
 }
