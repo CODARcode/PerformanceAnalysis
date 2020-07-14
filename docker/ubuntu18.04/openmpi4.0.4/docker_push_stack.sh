8e2eebe319ba5131fec6556dcd407b31cc8baeb3
Christopher Kelly <ckelly@bnl.gov>
2020-07-14 15:59:29 -0400

    Added bash scripts to build/push ubuntu18.04/openmpi4.0.4 docker images

---
+++ docker/ubuntu18.04/openmpi4.0.4/docker_push_stack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker push chimbuko/base:ubuntu18.04
+docker push chimbuko/adios2:ubuntu18.04
+docker push chimbuko/mochi:ubuntu18.04
+docker push chimbuko/ad:ubuntu18.04-provdb
+docker push chimbuko/tau2:ubuntu18.04
+
+
+
