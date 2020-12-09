5f818044d13cccd479b0b97323af47c8cb7cc8a1
Christopher Kelly <ckelly@bnl.gov>
2020-12-09 13:14:02 -0500

    Added dockerFiles to build the MOCU Python+GPU workflow and run with the full Chimbuko stack

--- docker/ubuntu18.04-cuda10.1/openmpi4.0.4/docker_push_stack.sh
+++ docker/ubuntu18.04-cuda10.1/openmpi4.0.4/docker_push_stack.sh
@@ -6,6 +6,7 @@ docker push chimbuko/tau2:ubuntu18.04-cuda10.1
 docker push chimbuko/ad:ubuntu18.04-cuda10.1-provdb
 docker push chimbuko/viz:ubuntu18.04-cuda10.1
 docker push chimbuko/run_examples:ubuntu18.04-cuda10.1-provdb
-
+docker push chimbuko/mocu:ubuntu18.04-cuda10.1-provdb
+docker push chimbuko/run_mocu:ubuntu18.04-cuda10.1-provdb
 
 
