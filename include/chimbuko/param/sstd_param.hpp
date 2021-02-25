81207ac0a8092f3ae89db77123b1494afb66d3ee
Sandeep Mittal <smittal2@binghamton.edu>
2021-02-25 11:07:25 -0500

    minor fixes

--- include/chimbuko/param/sstd_param.hpp
+++ include/chimbuko/param/sstd_param.hpp
@@ -155,7 +155,7 @@ namespace chimbuko {
       double glob_threshold;
       std::vector<int> counts;
       std::vector<double> bin_edges;
-    };
+    } data;
 
     void clear();
 
@@ -259,7 +259,7 @@ namespace chimbuko {
      * @brief Get an element of the internal map
      * @param id The global function index
      */
-    Histogram& get_hist(unsigned long id) { return m_hbosstats[id]; }
+    Histogram& operator [](unsigned long id) { return m_hbosstats[id]; }
 
     /**
      * @brief Convert a run statistics Cereal portable binary representation string into a map
