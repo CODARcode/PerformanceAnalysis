a7bc36b311bcf6a51eedd89ccea04d1614e39dd2
Sandeep Mittal <smittal2@binghamton.edu>
2021-02-26 09:41:46 -0500

    minor fixes

--- test/unit_tests/ad/ADOutlier.cpp
+++ test/unit_tests/ad/ADOutlier.cpp
@@ -26,6 +26,45 @@ public:
   }
 };
 
+class ADOutlierHBOSTest: public ADOutlierHBOS{
+public:
+  ADOutlierHBOSTest(ADOutlier::OutlierStatistic stat = ADOutlier::ExclusiveRuntime): ADOutlierHBOS(stat){}
+
+  std::pair<size_t, size_t> sync_param_test(ParamInterface* param){ return this->ADOutlierHBOS::sync_param(param); }
+
+  unsigned long compute_outliers_test(Anomalies &anomalies,
+				      const unsigned long func_id, std::vector<CallListIterator_t>& data){
+    return this->compute_outliers(anomalies,func_id, data);
+  }
+};
+
+TEST(HBOSADOutlierTestSyncParamWithoutPS, Works){
+  HbosParam local_params_ps;
+
+  std::default_random_engine gen;
+  std::normal_distribution<double> dist(500.,100.);
+  int N = 50;
+
+  std::unordered_map<unsigned long, Histogram> local_params_ps_in;
+  {
+    Histogram &h = local_params_ps_in[0];
+    std::vector<double> runtime;
+    for(int i=0;i<N;i++) runtime.push_back(dist(gen));
+    h.create_histogram(runtime);
+  }
+  local_params_ps.assign(local_params_ps_in);
+
+  std::cout << local_params_ps_in[0].get_json().dump();
+
+  ADOutlierHBOSTest outlier;
+  outlier.sync_param_test(&local_params_ps);
+
+  //internal copy should be equal to global copy
+  std::string in_state = outlier.get_global_parameters()->serialize();
+
+  EXPECT_EQ(local_params_ps.serialize(), in_state);
+}
+
 TEST(ADOutlierTestSyncParamWithoutPS, Works){
   SstdParam local_params_ps;
 
