2f31664c1cba9dbd803caffa2f0c2cdf148430ee
Sandeep Mittal <smittal2@binghamton.edu>
2021-02-26 09:34:32 -0500

    minor fixes

--- test/unit_tests/ad/ADOutlier.cpp
+++ test/unit_tests/ad/ADOutlier.cpp
@@ -16,10 +16,7 @@ using namespace chimbuko;
 //Derived class to allow access to protected member functions
 class ADOutlierSSTDTest: public ADOutlierSSTD{
 public:
-  ADOutlier::OutlierStatistic stat = ADOutlier::ExclusiveRuntime;
-  ADOutlierSSTDTest(stat): ADOutlierSSTD(stat){
-
-  }
+  ADOutlierSSTDTest(ADOutlier::OutlierStatistic stat = ADOutlier::ExclusiveRuntime): ADOutlierSSTD(stat){}
 
   std::pair<size_t, size_t> sync_param_test(ParamInterface* param){ return this->ADOutlierSSTD::sync_param(param); }
 
@@ -28,49 +25,6 @@ public:
     return this->compute_outliers(anomalies,func_id, data);
   }
 };
-
-class ADOutlierHBOSTest: public ADOutlierHBOS{
-public:
-  ADOutlier::OutlierStatistic stat = ADOutlier::ExclusiveRuntime;
-  ADOutlierSSTDTest(stat): ADOutlierHBOS(stat){
-
-  }
-
-  std::pair<size_t, size_t> sync_param_test(ParamInterface* param){ return this->ADOutlierHBOS::sync_param(param); }
-
-  unsigned long compute_outliers_test(Anomalies &anomalies,
-				      const unsigned long func_id, std::vector<CallListIterator_t>& data){
-    return this->compute_outliers(anomalies,func_id, data);
-  }
-};
-
-TEST(HBOSADOutlierTestSyncParamWithoutPS, Works){
-  HbosParam local_params_ps;
-
-  std::default_random_engine gen;
-  std::normal_distribution<double> dist(500.,100.);
-  int N = 50;
-
-  std::unordered_map<unsigned long, Histogram> local_params_ps_in;
-  {
-    Histogram &h = local_params_ps_in[0];
-    std::vector<double> runtime;
-    for(int i=0;i<N;i++) runtime.push_back(dist(gen));
-    h.create_histogram(runtime);
-  }
-  local_params_ps.assign(local_params_ps_in);
-
-  std::cout << local_params_ps_in[0].get_json().dump();
-
-  ADOutlierHBOSTest outlier;
-  outlier.sync_param_test(&local_params_ps);
-
-  //internal copy should be equal to global copy
-  std::string in_state = outlier.get_global_parameters()->serialize();
-
-  EXPECT_EQ(local_params_ps.serialize(), in_state);
-}
-
 
 TEST(ADOutlierTestSyncParamWithoutPS, Works){
   SstdParam local_params_ps;
