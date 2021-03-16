4d22eb0cb9e6e136a976fb30070ba81d95e372ac
sandeepmittal <smittalpro2@gmail.com>
2021-03-16 15:08:48 -0400

    factory methods addded

--- src/param.cpp
+++ src/param.cpp
@@ -9,20 +9,14 @@ ParamInterface::ParamInterface()
 }
 
 ParamInterface *ParamInterface::set_AdParam(const std::string & ad_algorithm) {
-  //std::string hbos = 'hbos', sstd = 'sstd';
-  switch(ad_algorithm) {
-    case 'hbos': return new HbosParam();
-    case 'sstd': return new SstdParam();
-    default:return nullptr;
+  const std::string hbos = 'hbos', sstd = 'sstd';
+  if (ad_algorithm.compare(hbos) == 0) {
+    return new HbosParam();
   }
-  //
-  // if (ad_algorithm.compare(hbos) == 0) {
-  //   return new HbosParam();
-  // }
-  // else if (ad_algorithm.compare(sstd) == 0) {
-  //   return new SstdParam();
-  // }
-  // else {
-  //   return nullptr;
-  // }
+  else if (ad_algorithm.compare(sstd) == 0) {
+    return new SstdParam();
+  }
+  else {
+    return nullptr;
+  }
 }
