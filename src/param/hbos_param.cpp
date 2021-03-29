0ca1bb1e39e9399a9115e6869517ab829b136aa4
sandeepmittal <smittalpro2@gmail.com>
2021-03-29 19:48:04 -0400

    binwidth = sqrt(size)

--- src/param/hbos_param.cpp
+++ src/param/hbos_param.cpp
@@ -203,7 +203,7 @@ using namespace chimbuko;
      }
      else{ // bin_width is > 0
        std::cout << "BindWidth is > 0 here: " << std::endl;
-       const int num_bins = ((max_runtime - min_runtime) / bin_width) + 1;
+       const int num_bins = ceil((max_runtime - min_runtime) / bin_width);
 
        //combined.add2binedges(min_runtime);
        //const int h = runtimes.size() - 1;
