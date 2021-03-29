d3b1aea5e2504f7f6f235cfcba6bc41a990f611e
sandeepmittal <smittalpro2@gmail.com>
2021-03-29 11:34:11 -0400

    friend operator+

--- include/chimbuko/param/hbos_param.hpp
+++ include/chimbuko/param/hbos_param.hpp
@@ -82,8 +82,8 @@ namespace chimbuko {
     /**
      * @brief Combine two Histogram instances such that the resulting statistics are the union of the two
      */
-    Histogram combine_two_histograms(const Histogram& g, const Histogram& l);
-    //void combine_two_histograms(Histogram& g, const Histogram& l);
+    //Histogram combine_two_histograms(const Histogram& g, const Histogram& l);
+    friend Histogram operator+(const Histogram g, const Histogram l);
 
     /**
      * @brief Combine two Histogram instances such that the resulting statistics are the union of the two
@@ -160,6 +160,7 @@ namespace chimbuko {
     static double _scott_binWidth(const std::vector<int> & global_counts, const std::vector<double> & global_edges, const std::vector<int> & local_counts, const std::vector<double> & local_edges);
 
   };
+  Histogram operator+(const Histogram g, const Histogram l);
 
   /**
    * @@brief Implementation of ParamInterface for HBOS based anomaly detection
--- src/param/hbos_param.cpp
+++ src/param/hbos_param.cpp
@@ -138,7 +138,8 @@ using namespace chimbuko;
  Histogram::Histogram(){}
  Histogram::~Histogram(){}
 
- Histogram Histogram::combine_two_histograms (const Histogram& g, const Histogram& l) {
+ //Histogram Histogram::combine_two_histograms (const Histogram& g, const Histogram& l) {
+ Histogram chimbuko::operator+ (const Histogram g, const Histogram l) {
    Histogram combined;
    double min_runtime = std::numeric_limits<double>::max(), max_runtime = 0;
    // std::cout << "Bin_Edges Size of Global Histogram: " << std::to_string(g.bin_edges().size()) << ", Bin_Edges Size of Local Histogram: " << std::to_string(l.bin_edges().size()) << std::endl;
@@ -207,10 +208,10 @@ using namespace chimbuko;
 
  Histogram& Histogram::operator+=(const Histogram& h)
  {
-    const Histogram combined = combine_two_histograms(*this, h);
-    this->clear();
+    //const Histogram combined = combine_two_histograms(*this, h);
+    Histogram combined = *this + h;
     *this = combined;
-    this->set_hist_data(Histogram::Data(this->get_threshold(), this->counts(), this->bin_edges()));
+    //this->set_hist_data(Histogram::Data(this->get_threshold(), this->counts(), this->bin_edges()));
     return *this;
  }
 
