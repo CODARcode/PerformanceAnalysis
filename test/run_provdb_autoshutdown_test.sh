006b402f89c5f56ecd78a81733c36c69e4a99530
Christopher Kelly <ckelly@bnl.gov>
2021-06-25 14:46:21 -0400

    ProvDB integration test scripts now query fi_info for ofi_rxm and fallback to sockets, allowing running with default spack install of libfabric, which does not include ofi_rxm

--- test/run_provdb_autoshutdown_test.sh
+++ test/run_provdb_autoshutdown_test.sh
@@ -4,14 +4,24 @@ set -e
 set -o pipefail
 
 if [ -f "../bin/provdb_admin" ]; then
-    #Connect via tcp
     rm -f provdb.*.unqlite*  provider.address
 
+    #Find a protocol supported by libfabric
+    protocol=
+    if [ $(fi_info -l | grep ofi_rxm | wc -l) == "1" ]; then
+	protocol="ofi+tcp"
+    elif [ $(fi_info -l | grep sockets | wc -l) == "1" ]; then
+	protocol="sockets"
+    else
+	"Neither tcp;ofi_rxm or sockets fabrics are supported by the install of libfabrics"
+	exit 1
+    fi
+    
     ip=$(hostname -i)
     port=1234
     shards=2
 
-    ../bin/provdb_admin ${ip}:${port} -autoshutdown true -nshards ${shards} &
+    ../bin/provdb_admin ${ip}:${port} -autoshutdown true -nshards ${shards} -engine "${protocol}" &
     admin=$!
     while [ ! -f provider.address ]; do
 	echo "Waiting for provider address"
--- test/run_provdb_pserver_client_test.sh
+++ test/run_provdb_pserver_client_test.sh
@@ -4,14 +4,27 @@ set -e
 set -o pipefail
 
 if [ -f "../bin/provdb_admin" ]; then
-    echo "Test 1 clients 1 pserver TCP"
     rm -f provdb.*.unqlite*  provider.address
 
+    #Find a protocol supported by libfabric
+    protocol=
+    if [ $(fi_info -l | grep ofi_rxm | wc -l) == "1" ]; then
+	protocol="ofi+tcp"
+    elif [ $(fi_info -l | grep sockets | wc -l) == "1" ]; then
+	protocol="sockets"
+    else
+	"Neither tcp;ofi_rxm or sockets fabrics are supported by the install of libfabrics"
+	exit 1
+    fi
+
+    echo "Test 1 clients 1 pserver ${protocol}"
+
+    
     ip=$(hostname -i)
     port=1234
     shards=1
 
-    ../bin/provdb_admin ${ip}:${port}  -autoshutdown true -nshards 1 &
+    ../bin/provdb_admin ${ip}:${port}  -autoshutdown true -nshards 1 -engine "${protocol}" &
     admin=$!
     while [ ! -f provider.address ]; do
 	echo "Waiting for provider address"
