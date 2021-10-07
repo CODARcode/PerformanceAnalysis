b11cc9400435267993a0f0b376fb18fe76225dd2
Christopher Kelly <ckelly@bnl.gov>
2021-10-07 09:40:22 -0400

    Improvements to provDB log parser script

--- benchmark_suite/benchmark_provdb/parse_provdb_prd_logs.py
+++ benchmark_suite/benchmark_provdb/parse_provdb_prd_logs.py
@@ -64,40 +64,28 @@ class rankData:
 
                 #print("%d %d %d %d" % (ts,lsteps,linc,ltot))
                 
-
-class shardBinnedData:
-    def __init__(self, t_start, bin_size, nbins):
+#Ranks will start at slightly different times but same cadence so we just combine by the data point index
+class shardData:
+    def __init__(self, tsep, ndata_largest):
         self.incomplete = []
         self.total = []
-        self.tbin = []  #start time of bin (not including t_start offset)
+        self.t = []  #start time of bin (not including t_start offset)
         self.serviced = []
         self.new_req = []
-        self.t_start = t_start
-        self.bin_size = bin_size
-        self.nbins = nbins
-        for b in range(nbins):
+        for b in range(ndata_largest):
             self.incomplete.append(0)
             self.total.append(0)
-            self.tbin.append( b*bin_size )
+            self.t.append( b*tsep )
             self.serviced.append(0)
             self.new_req.append(0)
 
     #r is a rankData object
-    def bin(self, r):
+    def insert(self, r):
         for i in range(len(r.times)):
-            b = int( float(r.times[i] - self.t_start)/ self.bin_size )
-            if b >= len(self.tbin):
-                print("Invalid bin %d" % b)
-                sys.exit(1)
-            self.incomplete[b] += r.incomplete[i]
-            self.total[b] += r.total[i]
-            self.serviced[b] += r.serviced[i]
-            self.new_req[b] += r.new_req[i]
-
-#chimbuko/logs/client_perf_prd.2.log
-#2021/07/06 09:42:39 io_steps:6 provdb_incomplete_async_sends:4 provdb_total_async_send_calls:12
-
-
+            self.incomplete[i] += r.incomplete[i]
+            self.total[i] += r.total[i]
+            self.serviced[i] += r.serviced[i]
+            self.new_req[i] += r.new_req[i]
 
 
 def readShardMap(filename, ranks, shards):
@@ -171,137 +159,132 @@ tsep = float(tt)/nn
 
 print("Avg seconds between writes: %f" % (tsep))
 
-bin_size = int(tsep) * 4
-
 #Find earliest and latest timestamp
 t_earliest = shard_data[0][0].times[0]
 t_latest = shard_data[0][0].times[-1]
-for s in range(1, shards):
+ndata_largest = 0 #largest number of data points over ranks
+for s in range(0, shards):
     for r in range(len(shard_data[s])):
         if shard_data[s][r].times[0] < t_earliest:
             t_earliest = shard_data[s][r].times[0]
         if shard_data[s][r].times[-1] > t_latest:
             t_latest = shard_data[s][r].times[-1]
+        if len(shard_data[s][r].times) > ndata_largest:
+            ndata_largest = len(shard_data[s][r].times)
 
 print("Earliest and latest timestamp %d %d" % (t_earliest, t_latest) )
 
-bins = int( float(t_latest - t_earliest)/bin_size ) + 1
-
 #Compute rates by shard
 #Use only the data in the middle 50% of the run to avoid the startup and shutdown effects
-avg_rate_by_shard = [ None for s in range(shards) ]
+# avg_rate_by_shard = [ None for s in range(shards) ]
 
-avg_rank_rate_by_shard = [ None for s in range(shards) ]
-max_rank_rate_by_shard = [ None for s in range(shards) ]
-min_rank_rate_by_shard = [ None for s in range(shards) ]
-stddev_rank_rate_by_shard = [ None for s in range(shards) ]
-
-
-for s in range(shards):
-    rates = []
-    for rdata in shard_data[s]:
-        start = rdata.ndata / 4
-        end = 3*rdata.ndata/4
-
-        print("Start %d end %d" % (start,end))
-        for i in range(start, end):
-            tstep=0
-            if(i==0):
-                #Assume tstep is uniform
-                tstep = rdata.times[1] - rdata.times[0]
-            else:
-                tstep = rdata.times[i] - rdata.times[i-1]
-            rates.append( float(rdata.serviced[i])/tstep )
-            print("%f " % rates[-1],end='')
-        print("")
+# avg_rank_rate_by_shard = [ None for s in range(shards) ]
+# max_rank_rate_by_shard = [ None for s in range(shards) ]
+# min_rank_rate_by_shard = [ None for s in range(shards) ]
+# stddev_rank_rate_by_shard = [ None for s in range(shards) ]
 
 
-    avg_rate = 0
-    avg_rate2 = 0
-    min_rate = rates[0]
-    max_rate = rates[0]
-    for r in rates:
-        avg_rate += r / len(rates)
-        avg_rate2 += r*r / len(rates)
-        if r < min_rate:
-            min_rate = r
-        if r > max_rate:
-            max_rate = r
+# for s in range(shards):
+#     rates = []
+#     for rdata in shard_data[s]:
+#         start = int(rdata.ndata / 4)
+#         end = int(3*rdata.ndata/4)
 
-    avg_rank_rate_by_shard[s] = avg_rate
-    stddev_rank_rate_by_shard[s] = math.sqrt( avg_rate2 - avg_rate * avg_rate )
-    min_rank_rate_by_shard[s] = min_rate
-    max_rank_rate_by_shard[s] = max_rate
+#         print("Start %d end %d" % (start,end))
+#         for i in range(start, end):
+#             tstep=0
+#             if(i==0):
+#                 #Assume tstep is uniform
+#                 tstep = rdata.times[1] - rdata.times[0]
+#             else:
+#                 tstep = rdata.times[i] - rdata.times[i-1]
+#             rates.append( float(rdata.serviced[i])/tstep )
+#             print("%f " % rates[-1],end='')
+#         print("")
 
-    #This is the average rate per shard, multiply by number of ranks to get average rate for shard
-    avg_rate_by_shard[s] = avg_rate * len(shard_data[s])
+
+#     avg_rate = 0
+#     avg_rate2 = 0
+#     min_rate = rates[0]
+#     max_rate = rates[0]
+#     for r in rates:
+#         avg_rate += r / len(rates)
+#         avg_rate2 += r*r / len(rates)
+#         if r < min_rate:
+#             min_rate = r
+#         if r > max_rate:
+#             max_rate = r
+
+#     avg_rank_rate_by_shard[s] = avg_rate
+#     stddev_rank_rate_by_shard[s] = math.sqrt( avg_rate2 - avg_rate * avg_rate )
+#     min_rank_rate_by_shard[s] = min_rate
+#     max_rank_rate_by_shard[s] = max_rate
+
+#     #This is the average rate per shard, multiply by number of ranks to get average rate for shard
+#     avg_rate_by_shard[s] = avg_rate * len(shard_data[s])
     
-print("Avg per-rank rates by shard (middle 50%)")
-for s in range(shards):
-    print("(%.1f +- %.1f) " % (avg_rank_rate_by_shard[s], stddev_rank_rate_by_shard[s])  , end='')
-print("")
+# print("Avg per-rank rates by shard (middle 50%)")
+# for s in range(shards):
+#     print("(%.1f +- %.1f) " % (avg_rank_rate_by_shard[s], stddev_rank_rate_by_shard[s])  , end='')
+# print("")
 
-print("Min/max per-rank rates by shard (middle 50%)")
-for s in range(shards):
-    print("(%.1f %.1f)" % (min_rank_rate_by_shard[s], max_rank_rate_by_shard[s]) , end='')
-print("")
+# print("Min/max per-rank rates by shard (middle 50%)")
+# for s in range(shards):
+#     print("(%.1f %.1f)" % (min_rank_rate_by_shard[s], max_rank_rate_by_shard[s]) , end='')
+# print("")
 
-
-
-
-
-print("Average rates by shard (middle 50%): ",end='')
-for s in range(shards):
-    print("%f " % avg_rate_by_shard[s], end='')
-print("")
+# print("Average rates by shard (middle 50%): ",end='')
+# for s in range(shards):
+#     print("%f " % avg_rate_by_shard[s], end='')
+# print("")
 
 
 #Bin data over time for all ranks in a shard because the timestamps won't match
-print("Binning into %d bins of size %d" % (bins, bin_size))
+print("Combining ranks by shard")
 
-shard_data_binned = [ shardBinnedData(t_earliest, bin_size, bins) for i in range(shards) ]
+shard_data_combined = [ shardData(tsep, ndata_largest) for i in range(shards) ]
 for s in range(shards):
   for r in shard_data[s]:
-      shard_data_binned[s].bin(r)
+      shard_data_combined[s].insert(r)
 
-print("Number of requests serviced in time bin and number of new requests by shard")
-for b in range(bins):
-    print("%d " % shard_data_binned[0].tbin[b], end='')
+print("Number of requests serviced and number of new requests by shard as a function of time")
+for b in range(ndata_largest):
+    print("%d " % shard_data_combined[0].t[b], end='')
     for s in range(shards):
-        print("%d/%d " % (shard_data_binned[s].serviced[b], shard_data_binned[s].new_req[b]), end='')
+        print("%d/%d " % (shard_data_combined[s].serviced[b], shard_data_combined[s].new_req[b]), end='')
     print("")
 
 
 print("Number of requests/second serviced and new requests per second in time bin by shard")
-for b in range(bins):
-    print("%d " % shard_data_binned[0].tbin[b], end='')
+for b in range(ndata_largest):
+    print("%d " % shard_data_combined[0].t[b], end='')
     for s in range(shards):
-        print("%.1f/%.1f " % ( float(shard_data_binned[s].serviced[b])/bin_size,  float(shard_data_binned[s].new_req[b])/bin_size   )  , end='')
+        print("%.1f/%.1f " % ( float(shard_data_combined[s].serviced[b])/tsep,  float(shard_data_combined[s].new_req[b])/tsep   )  , end='')
     print("")
 
 
 print("Defecit/surplus number of requests serviced in time bin (serviced - new) by shard")
-for b in range(bins):
-    print("%d " % shard_data_binned[0].tbin[b], end='')
+for b in range(ndata_largest):
+    print("%d " % shard_data_combined[0].t[b], end='')
     for s in range(shards):
-        print("%d " % (shard_data_binned[s].serviced[b]- shard_data_binned[s].new_req[b]), end='')
+        print("%d " % (shard_data_combined[s].serviced[b]- shard_data_combined[s].new_req[b]), end='')
     print("")
 
 #Add the defecits to a file that can be parsed by xmgrace
 f = open("defecits.dat", 'w')
 for s in range(shards):
-    for b in range(bins):
-        f.write("%d %d\n" % (shard_data_binned[s].tbin[b],   shard_data_binned[s].serviced[b]- shard_data_binned[s].new_req[b]) )
+    for b in range(ndata_largest):
+        f.write("%d %d\n" % (shard_data_combined[s].t[b],   shard_data_combined[s].serviced[b]- shard_data_combined[s].new_req[b]) )
     f.write("\n")
 f.close()
 
 print("Defecit/surplus number of requests serviced in time bin (serviced - new) total")
 f = open("defecits_total.dat", 'w')
-for b in range(bins):
+for b in range(ndata_largest):
     defecit=0
     for s in range(shards):
-        defecit += shard_data_binned[s].serviced[b]- shard_data_binned[s].new_req[b]
-    rr="%d %d" % (shard_data_binned[0].tbin[b], defecit)
+        defecit += shard_data_combined[s].serviced[b]- shard_data_combined[s].new_req[b]
+    rr="%d %d" % (shard_data_combined[0].t[b], defecit)
     print(rr)
     f.write(rr + "\n")
 f.close()
@@ -310,15 +293,54 @@ f.close()
 print("Aggregate Defecit/surplus number of requests serviced as a function of time (sum of defecits to now)")
 f = open("defecits_aggregate_total.dat", 'w')
 defecit_sum=0
-for b in range(bins):
+for b in range(ndata_largest):
     defecit=0
     for s in range(shards):
-        defecit += shard_data_binned[s].serviced[b]- shard_data_binned[s].new_req[b]
+        defecit += shard_data_combined[s].serviced[b]- shard_data_combined[s].new_req[b]
     defecit_sum += defecit
-    rr="%d %d" % (shard_data_binned[0].tbin[b], defecit_sum)
+    rr="%d %d" % (shard_data_combined[0].t[b], defecit_sum)
     print(rr)
     f.write(rr + "\n")
 f.close()
+
+
+print("Requests serviced in time bin total")
+f = open("serviced_total.dat", 'w')
+for b in range(ndata_largest):
+    serviced=0
+    for s in range(shards):
+        serviced += shard_data_combined[s].serviced[b]
+    rr="%d %d" % (shard_data_combined[0].t[b], serviced)
+    print(rr)
+    f.write(rr + "\n")
+f.close()
+
+
+print("Aggregate requests serviced as a function of time (sum of serviced to now)")
+f = open("serviced_aggregate_total.dat", 'w')
+serviced_sum=0
+for b in range(ndata_largest):
+    serviced=0
+    for s in range(shards):
+        serviced += shard_data_combined[s].serviced[b]
+    serviced_sum += serviced
+    rr="%d %d" % (shard_data_combined[0].t[b], serviced_sum)
+    print(rr)
+    f.write(rr + "\n")
+f.close()
+
+
+print("Incomplete requests as a function of time total")
+f = open("incomplete_total.dat", 'w')
+for b in range(ndata_largest):
+    incomplete=0
+    for s in range(shards):
+        incomplete += shard_data_combined[s].incomplete[b]
+    rr="%d %d" % (shard_data_combined[0].t[b], incomplete)
+    print(rr)
+    f.write(rr + "\n")
+f.close()
+
 
 
 ######################## rank 0 analysis #############################
@@ -353,6 +375,15 @@ for d in range(len(rank0.times)):
     defecit= rank0.serviced[d]- rank0.new_req[d]
     defecit_sum += defecit
     rr="%d %d" % (rank0.times[d] - rank0.times[0], defecit_sum)
+    print(rr)
+    f.write(rr + "\n")
+f.close()
+
+print("Incomplete requests as a function of time for rank 0")
+f = open("incomplete_total_rank0.dat", 'w')
+for d in range(len(rank0.times)):
+    incomplete= rank0.incomplete[d]
+    rr="%d %d" % (rank0.times[d] - rank0.times[0], incomplete)
     print(rr)
     f.write(rr + "\n")
 f.close()
