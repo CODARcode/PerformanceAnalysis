9aa1d2e0a1bd4f6021479794ec53ca68169649b7
Christopher Kelly <ckelly@bnl.gov>
2021-11-08 12:56:37 -0500

    In services script disabled write check for adios2 path as it prevents offline analysis of read-only data. Need a better solution for the future.

--- scripts/launch/run_services.sh
+++ scripts/launch/run_services.sh
@@ -61,12 +61,12 @@ var_dir=${base}/chimbuko/vars
 bp_dir=${base}/chimbuko/adios2
 
 #Check tau adios2 path is writable; by default this is ${bp_dir} but it can be overridden by users, eg for offline analysis
-touch ${TAU_ADIOS2_PATH}/write_check
-if [[ $? == 1 ]]; then
-    echo "Chimbuko Services: Could not write to ADIOS2 output path ${TAU_ADIOS2_PATH}, check permissions"
-    exit 1
-fi
-rm -f ${TAU_ADIOS2_PATH}/write_check
+#touch ${TAU_ADIOS2_PATH}/write_check
+#if [[ $? == 1 ]]; then
+#    echo "Chimbuko Services: Could not write to ADIOS2 output path ${TAU_ADIOS2_PATH}, check permissions"
+#    exit 1
+#fi
+#rm -f ${TAU_ADIOS2_PATH}/write_check
 
 #Get head node IP
 if command -v ip &> /dev/null
