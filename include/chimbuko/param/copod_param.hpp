6f2453b26ef74549aa57c30db356c6e2ba7bf648
Christopher Kelly <ckelly@bnl.gov>
2022-03-17 10:56:56 -0400

    Moved Histogram class into its own header/cpp file

--- include/chimbuko/param/copod_param.hpp
+++ include/chimbuko/param/copod_param.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include <chimbuko_config.h>
 #include "chimbuko/param.hpp"
-#include "chimbuko/param/hbos_param.hpp"
 #include "chimbuko/util/RunStats.hpp"
+#include "chimbuko/util/Histogram.hpp"
 #include <unordered_map>
 #include <nlohmann/json.hpp>
 #include <vector>
