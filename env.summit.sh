6be55788fcc962a0bbb4ae9191404328afcfbe90
Christopher Kelly <ckelly@bnl.gov>
2023-10-10 16:32:05 -0400

    Removed some old, deprecated shell scripts

--- env.summit.sh
+++
@@ -1,3 +0,0 @@
-module reset
-module load gcc/9.1.0 papi/5.7.0 cmake/3.18.2 openssl/1.0.2 zeromq/4.2.5 zlib/1.2.11 c-blosc/1.12.1 bzip2/1.0.6 sz/2.0.2.0 hdf5 python/3.7.0
-module unload darshan-runtime/3.1.7
