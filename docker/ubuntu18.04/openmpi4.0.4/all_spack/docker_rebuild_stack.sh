70197594a4f3be3d5b9358638412d17b9ac34f24
Christopher Kelly <ckelly@bnl.gov>
2024-04-30 09:30:23 -0400

    Added a complete Docker build pathway via Spack, dramatically simplifying the build procedure

---
+++ docker/ubuntu18.04/openmpi4.0.4/all_spack/docker_rebuild_stack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+#docker build -f Dockerfile.spack_env -t chimbuko/chimbuko-spack-env:ubuntu18.04 .
+#docker build -f Dockerfile.spack_release -t chimbuko/chimbuko-spack-release:ubuntu18.04 .
+docker build -f Dockerfile.playground -t chimbuko/chimbuko-spack-playground:ubuntu18.04 .
