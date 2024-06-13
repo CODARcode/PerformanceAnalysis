4b3244f21b933b3c3f1d65d843a8bf5230d18d01
Christopher Kelly <ckelly@bnl.gov>
2024-06-13 16:56:05 -0400

    Added a Dockerfile for a development image for Chimbuko, maintaining the spack environment and installing useful tooling, intended for use with 'spack develop'

--- docker/ubuntu18.04/openmpi4.0.4/all_spack/docker_rebuild_stack.sh
+++ docker/ubuntu18.04/openmpi4.0.4/all_spack/docker_rebuild_stack.sh
@@ -4,4 +4,5 @@ set -e
 
 #docker build -f Dockerfile.spack_env -t chimbuko/chimbuko-spack-env:ubuntu18.04 .
 #docker build -f Dockerfile.spack_release -t chimbuko/chimbuko-spack-release:ubuntu18.04 .
-docker build -f Dockerfile.playground -t chimbuko/chimbuko-spack-playground:ubuntu18.04 .
+#docker build -f Dockerfile.playground -t chimbuko/chimbuko-spack-playground:ubuntu18.04 .
+docker build -f Dockerfile.spack_dev -t chimbuko/chimbuko-spack-dev:ubuntu18.04 .
