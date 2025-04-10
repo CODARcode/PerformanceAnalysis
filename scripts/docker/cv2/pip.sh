d31dcf56ee4f7534f869a5a16a9b44ee74b7a7c9
Dakota Blair <dblair@bnl.gov>
2025-04-10 13:48:16 -0400

    Ensure pip with ensurepip.

--- scripts/docker/cv2/pip.sh
+++ scripts/docker/cv2/pip.sh
@@ -6,4 +6,5 @@ source /spack/spack/share/spack/setup-env.sh
 cd /opt/spack-environment/
 spack env activate .
 cd /opt/spack-environment/chimbuko-visualization2/
-pip3 install -r requirements.txt
+python3 -m ensurepip
+python3 -m pip install -r requirements.txt
